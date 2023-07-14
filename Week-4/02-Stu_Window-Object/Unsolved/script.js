552e9f168aab9247f3b6c09160d964967e3a617f
khinoj <kristopher.hinojosa@gmail.com>
2023-07-13 21:36:09 -0400

    week 4 day 1

---
+++ Week-4/02-Stu_Window-Object/Unsolved/script.js
@@ -0,0 +1,12 @@
+// TODO: What will the following line of code log? show window in console
+console.log(window);
+
+// TODO: What will the following line of code log? show the entire document
+console.log(window.document);
+
+// TODO: What will the following line of code log? root element html element
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log? display head element
+console.log(document.head);
+
---
+++ Week-4/08-Stu_Create-Append/Unsolved/script.js
@@ -0,0 +1,57 @@
+var body = document.body;
+var h1El = document.createElement("h1");
+var infoEl = document.createElement("div");
+var imgEl = document.createElement("img");
+var kittenEl = document.createElement("div");
+var nameEl = document.createElement("div");
+var favoriteEl = document.createElement("div");
+// Create ordered list element
+var listEl = document.createElement("ol");
+// Create ordered list items
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+h1El.textContent = "Welcome to my page";
+kittenEl.textContent = "This is my kitten 🐱.";
+nameEl.textContent = "His name is Jax.";
+favoriteEl.textContent = "My favorite foods are:";
+
+body.appendChild(h1El);
+body.appendChild(infoEl);
+infoEl.appendChild(imgEl);
+infoEl.appendChild(kittenEl);
+infoEl.appendChild(nameEl);
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+
+h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+imgEl.setAttribute("src", "http://placekitten.com/200/300");
+nameEl.setAttribute("style", "font-size:25px; text-align:center;");
+kittenEl.setAttribute("style", "font-size:25px; text-align:center;");
+favoriteEl.setAttribute("style", "font-size:20px;");
+listEl.setAttribute('style', 'background-color: #333333; padding: 20px;')
+
+
+// TODO: Add ordered list items containing four favorite foods
+//text content
+li1.textContent = "Pizza";
+li2.textContent = "Raman";
+li3.textContent = "Mexican Food";
+li4.textContent = "Pad Thai";
+
+// Append ordered list 
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+li1.setAttribute('style', 'color: white; padding: 5px; margin-left: 35px; background-color: #A9A9A9');
+
+li2.setAttribute('style', 'color: white; padding: 5px; margin-left: 35px; background-color: #BDB76B');
+
+li3.setAttribute('style', 'color: white; padding: 5px; margin-left: 35px; background-color: #2F4F4F');
+
+li4.setAttribute('style', 'color: white; padding: 5px; margin-left: 35px; background-color: #1E90FF');
