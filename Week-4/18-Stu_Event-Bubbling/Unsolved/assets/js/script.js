f36a0e3ca3bb3e22f27c37ce6f499aca3bcc934f
khinoj <kristopher.hinojosa@gmail.com>
2023-07-17 21:39:29 -0400

    week 4 day 2

--- Week-4/18-Stu_Event-Bubbling/Unsolved/assets/js/script.js
+++ Week-4/18-Stu_Event-Bubbling/Unsolved/assets/js/script.js
@@ -1,10 +1,10 @@
-// TODO: Which element is the following line of code selecting?
+// TODO: Which element is the following line of code selecting? div element 
 var carousel = document.querySelector(".carouselbox");
-// TODO: Which element is the following line of code selecting?
-var next = carousel.querySelector(".next");
-var prev = carousel.querySelector(".prev");
-var index = 0;
-var currentImage;
+// TODO: Which element is the following line of code selecting? 
+var next = carousel.querySelector(".next"); //button
+var prev = carousel.querySelector(".prev"); //button
+var index = 0;  //reg variable
+var currentImage; //variable
 
 var images = [
   "https://picsum.photos/300/200",
@@ -26,22 +26,22 @@ function navigate(direction) {
   carousel.style.backgroundImage = "url('" + currentImage + "')";
 }
 
-// TODO: Describe the functionality of the following event listener.
+// TODO: Describe the functionality of the following event listener. changes DOM url for next or prev pic. Source url for each picture. 
 carousel.addEventListener("click", function() {
   window.location.href = images[index];
 });
 
-// TODO: Describe the functionality of the following event listener.
+// TODO: Describe the functionality of the following event listener. next button
 next.addEventListener("click", function(event) {
-  // TODO: What is the purpose of the following line of code?
+  // TODO: What is the purpose of the following line of code? prevents propagation of the same event from being called .
   event.stopPropagation();
 
   navigate(1);
 });
 
-// TODO: Describe the functionality of the following event listener.
+// TODO: Describe the functionality of the following event listener. previous button
 prev.addEventListener("click", function(event) {
-    // TODO: What would happen if we didn't add the following line of code?
+    // TODO: What would happen if we didn't add the following line of code? line 31 will run. 
   event.stopPropagation();
 
   navigate(-1);
