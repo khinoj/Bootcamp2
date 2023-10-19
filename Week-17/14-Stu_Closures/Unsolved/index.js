4a7b6e4e8b77e4f7738abdbc75cd064b2ccd16db
khinoj <kristopher.hinojosa@gmail.com>
2023-10-19 19:57:00 -0400

    day 2 part

--- Week-17/14-Stu_Closures/Unsolved/index.js
+++ Week-17/14-Stu_Closures/Unsolved/index.js
@@ -1,4 +1,12 @@
 // TODO: Complete the 'counter' function below.
-function counter() {}
+function counter() {
+    let count = 0;
+    return {
+        increment: function () {
+        count += 1;
+        return count;
+      },
+    };
+}
 
 module.exports = counter;
